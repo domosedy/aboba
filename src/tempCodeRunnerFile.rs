8f049542f6098855c40389b3f2f74d69dad2cf55
sielu <ilyavin05@mail.ru>
2023-05-09 15:01:36 +0300

    My first commit

--- src/tempCodeRunnerFile.rs
+++
@@ -1,8 +0,0 @@
-                    // if let Some(vl) = response {
-                    //     match vl.value {
-                    //         None => return Err(CellId::Compute(current_cell)),
-                    //         Some(x) => x
-                    //     }
-                    // } else {
-                    //     return Err(CellId::Compute(current_cell));
-                    // }
